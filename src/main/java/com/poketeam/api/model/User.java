4203d3ee3364a939850b260d5eee9c02dad8addb
terrylh22 <63936327+terrylh22@users.noreply.github.com>
2025-04-14 19:20:45 -0400

    Added get user by username endpoint

--- src/main/java/com/poketeam/api/model/User.java
+++ src/main/java/com/poketeam/api/model/User.java
@@ -2,14 +2,17 @@ package com.poketeam.api.model;
 
 import lombok.Data;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
 
 @Data
 @Table("users")
 public class User {
     @Id
-    private Long user_id;
-    private String user_name;
+    @Column("user_id")
+    private Long userId;
+    @Column("user_name")
+    private String username;
     private String password;
     private String email;
 }
